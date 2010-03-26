7a3ffdab8db5d599941410d53b2527aa61d390e9
Josh Ourisman <josh@joshourisman.com>
2010-03-26 16:44:43 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Adamanteus',
+      version='0.1a',
+      description='Database Backups with Version Control',
+      author='Josh Ourisman',
+      author_email='josh@joshourisman.com',
+      url='http://www.bitbucket.org/Josh/adamanteus/',
+      scripts=['adamanteus.py',],)
