5f4d362b4ec68b2984c0e3b951421061871d7ef3
Josh Ourisman <josh@joshourisman.com>
2010-03-26 16:57:59 -0400

    set version number to 0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Adamanteus',
-      version='0.1a',
+      version='0.1',
       description='Database Backups with Version Control',
       author='Josh Ourisman',
       author_email='josh@joshourisman.com',
