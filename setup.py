3c07c0e5b9cc1f104b4ecd78d901734fe20999dd
Josh Ourisman <josh@joshourisman.com>
2010-03-26 18:47:00 -0400

    added more info to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,17 @@ setup(name='Adamanteus',
       description='Database Backups with Version Control',
       author='Josh Ourisman',
       author_email='josh@joshourisman.com',
+      license='BSD',
       url='http://www.bitbucket.org/Josh/adamanteus/',
+      download_url=("http://bitbucket.org/Josh/adamanteus/get/v0.1.tar.bz2"),
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        ],
+      platforms=['any'],
+      requires=['mercurial'],
       scripts=['adamanteus.py',],)
+
